d7c95dbe51b7154ece363c1fbb97871446c39f71
Joshua Randall <josh@joshrandall.net>
2026-01-14 22:43:56 -0500

    figuring out frontend

---
+++ frontend/src/Index.tsx
@@ -0,0 +1,15 @@
+
+
+function Index() {
+    return (
+    <>
+      <div>
+        <h1>Project Tracker</h1>
+        <button>Sign In</button>
+        <button>Sign Up</button>
+      </div>
+    </>
+)
+}
+
+export default Index
